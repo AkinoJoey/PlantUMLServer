0d1592e9a8534351d2b52a5feb04acf7223ec2db
AkinoJoey <gakiya213@gmail.com>
2023-09-11 16:52:34 +0900

    encodeの処理を追加

--- js/main.js
+++ js/main.js
@@ -4,24 +4,33 @@ const pngBtn = document.getElementById('png-btn');
 const svgBtn = document.getElementById('svg-btn');
 const asciiBtn = document.getElementById('ascii-btn');
 
-require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.41.0/min/vs' }});
-    
-require(['vs/editor/editor.main'], function() {
+require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.41.0/min/vs' } });
+
+require(['vs/editor/editor.main'], function () {
     const editor = monaco.editor.create(document.getElementById('editor-container'), {
-        value: '<!-- ここにテキスト入力してください -->\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n',
+        value: '@startuml \nBob -> Alice : hello\n@enduml',
         language: 'plaintext',
-        automaticLayout: true 
+        automaticLayout: true
     });
 
-    editor.onDidChangeModelContent(function(event) {
+
+    editor.onDidChangeModelContent(async function (event) {
         const plantUmlText = editor.getValue();
-
-        previewContainer.innerHTML = plantUmlText;
-
+        const encoded = await encode(plantUmlText);
+        console.log(encoded);
         
     });
 
 });
 
 
-
+async function encode(text){
+    let res = await fetch('encode.php',{
+        method:'POST',
+        headers:{'Content-Type': 'text/plain'},
+        body:text
+    })
+    .then(response => response.text())
+    
+    return res;
+}
