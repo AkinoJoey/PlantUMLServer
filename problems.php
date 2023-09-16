e670bb43e13ad4cd60684a7768957c554d69734a
AkinoJoey <gakiya213@gmail.com>
2023-09-16 11:24:42 +0900

    id変数を修正、title変数を追加

--- problems.php
+++ problems.php
@@ -3,10 +3,11 @@ require __DIR__ . '/vendor/autoload.php';
 
 use function Jawira\PlantUml\encodep;
 
-$id = $_POST['id'] ?? 1;
-
 $json = file_get_contents('./problems/problems.json');
 $data = json_decode($json, true);
+
+$id = $_GET['id'] ?? 0;
+$title = $data[$id]['title'];
 $uml = $data[$id]['uml'];
 $encode = encodep($uml);
 $encodeUrl = "https://www.plantuml.com/plantuml/png/{$encode}";
@@ -20,10 +21,10 @@ $encodeUrl = "https://www.plantuml.com/plantuml/png/{$encode}";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://unpkg.com/chota@latest">
     <link rel="stylesheet" href="css/style.css">
-    <title>UML問題集</title>
+    <title><?php echo $title ?></title>
 </head>
 <body>
-    <h2 class="text-center">PlantUML 問題</h2>
+    <h2 class="text-center"><?php echo $title ?></h2>
     <div class="is-center mb-1">
         <a id="answer-btn" class="button secondary" >Show Answer</a>
     </div>
@@ -37,7 +38,7 @@ $encodeUrl = "https://www.plantuml.com/plantuml/png/{$encode}";
         </div>
         <div class="col-4">
             <div id="answer-container"  class="monaco-container" >
-                <img  src="<?php echo $encodeUrl ?>">
+                <img src="<?php echo $encodeUrl ?>">
                 <p class="is-hidden"><?php echo $uml ?></p>
             </div>
         </div>
